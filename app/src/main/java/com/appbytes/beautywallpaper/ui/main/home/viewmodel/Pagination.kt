6584164c25219054af1c3025d349b357f62cfb33
Md Marjan Hossain <61978371+mdmarjanhossain0@users.noreply.github.com>
2021-06-18 15:17:40 +0600

    Complete Home Fragment

---
+++ app/src/main/java/com/appbytes/beautywallpaper/ui/main/home/viewmodel/Pagination.kt
@@ -0,0 +1,48 @@
+package com.appbytes.beautywallpaper.ui.main.home.viewmodel
+
+import android.util.Log
+import com.appbytes.beautywallpaper.ui.main.home.state.HomeStateEvent
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+
+
+
+
+@FlowPreview
+@UseExperimental(ExperimentalCoroutinesApi::class)
+private fun HomeViewModel.incrementPageNumber(){
+    val update = getCurrentViewStateOrNew()
+    Log.d(TAG, "current page number " + getCurrentViewStateOrNew().imageFields.page_number)
+    val page = update.copy().imageFields.page_number ?: 1
+    update.imageFields.page_number = page.plus(1)
+    setViewState(update)
+}
+
+@FlowPreview
+@UseExperimental(ExperimentalCoroutinesApi::class)
+fun HomeViewModel.nextPage(){
+    if(!isJobAlreadyActive(HomeStateEvent.GetNewPhotos())) {
+        Log.d(TAG, "HomeViewModel: Attempting to load next page...")
+        incrementPageNumber()
+        Log.d(TAG, "next page set page number " + getCurrentViewStateOrNew().imageFields.page_number)
+        setStateEvent(HomeStateEvent.GetNewPhotos(
+                page_number = getCurrentViewStateOrNew().imageFields.page_number ?: 1
+        ))
+    }
+}
+
+@FlowPreview
+@UseExperimental(ExperimentalCoroutinesApi::class)
+fun HomeViewModel.refreshFromCache(){
+    if(!isJobAlreadyActive(HomeStateEvent.GetNewPhotos())){
+        setStateEvent(HomeStateEvent.GetNewPhotos(clearLayoutManagerState = false))
+    }
+}
+
+@FlowPreview
+@UseExperimental(ExperimentalCoroutinesApi::class)
+fun HomeViewModel.cacheData(){
+    if(!isJobAlreadyActive(HomeStateEvent.GetNewPhotos())){
+        setStateEvent(HomeStateEvent.CacheData())
+    }
+}
